4f80a4e5329c55740d3c4876512ae6b25ab9e5ec
Dasa122 <153184061+Dasa122@users.noreply.github.com>
2024-01-31 14:37:39 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,17 +10,24 @@ datalogger.deleteLog()
 datalogger.mirrorToSerial(true)
 basic.forever(function () {
     if (1 == start) {
-        datalogger.log(datalogger.createCV("Sorszam", sorszam))
-        datalogger.log(datalogger.createCV("Paratlanszam", num))
+        datalogger.log(
+        datalogger.createCV("Sorszam", sorszam),
+        datalogger.createCV("Negyzetszam", num1 * num1)
+        )
+        sorszam += 1
+        num1 += 1
+        basic.pause(500)
+    }
+    if (1 == start) {
+        datalogger.log(
+        datalogger.createCV("Sorszam", sorszam),
+        datalogger.createCV("Paratlanszam", num)
+        )
         sorszam += 1
         num += 2
         basic.pause(500)
     }
-    if (1 == start) {
-        datalogger.log(datalogger.createCV("Sorszam", sorszam))
-        datalogger.log(datalogger.createCV("Negyzetszam", num1 * num1))
-        sorszam += 1
-        num1 += 1
-        basic.pause(500)
+    if (Math.sqrt(num) == Math.round(Math.sqrt(num))) {
+    	
     }
 })
