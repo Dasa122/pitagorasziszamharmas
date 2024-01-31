8499a6063500cd7872298e9f4453aa6a810ccf0b
Dasa122 <153184061+Dasa122@users.noreply.github.com>
2024-01-31 11:50:48 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 datalogger.onLogFull(function () {
     start = 0
 })
+let sorszam = 0
 let start = 0
 start = 1
 let num = 1
@@ -9,10 +10,17 @@ datalogger.deleteLog()
 datalogger.mirrorToSerial(true)
 basic.forever(function () {
     if (1 == start) {
-        datalogger.log(datalogger.createCV("Negyzetszam", num1 * num1))
+        datalogger.log(datalogger.createCV("Sorszam", sorszam))
         datalogger.log(datalogger.createCV("Paratlanszam", num))
+        sorszam += 1
         num += 2
+        basic.pause(500)
+    }
+    if (1 == start) {
+        datalogger.log(datalogger.createCV("Sorszam", sorszam))
+        datalogger.log(datalogger.createCV("Negyzetszam", num1 * num1))
+        sorszam += 1
         num1 += 1
-        basic.pause(1000)
+        basic.pause(500)
     }
 })
